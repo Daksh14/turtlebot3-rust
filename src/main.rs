afd61adfae12f89f76ade3394e93f9dae69b8339
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-03-29 21:10:40 -0400

    add sequencing

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod errors;
 
 mod yolo;
 
+use std::thread::current;
+
 use futures_core::Stream;
 use r2r::QosProfile;
 use r2r::{Node, Publisher};
@@ -31,14 +33,46 @@ pub fn create_pub_sub<T: r2r::WrappedTypesupport + 'static>(
     Ok((publisher, subscriber))
 }
 
+enum Sequence {
+    Intial360Rotation,
+    // Start randomly moving in x, y direction
+    RandomMovement,
+    // If charm is located, start moving towards it
+    TrackingToCharm,
+    // Charm is collected
+    SharmCollected,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut node = generate_node("nav_node")?;
     let publisher = node.create_publisher("/cmd_vel", QosProfile::default())?;
 
     let handle = tokio::task::spawn_blocking(move || {
+        let current_sequence = Sequence::Intial360Rotation;
+
         loop {
             node.spin_once(std::time::Duration::from_millis(100));
+        }
+    });
+
+    tokio::task::spawn(async move || {
+        loop {
+            match current_sequence {
+                Sequence::Intial360Rotation => {}
+                Sequence::RandomMovement => {
+                    // RandomMovement
+                }
+                Sequence::TrackingToCharm => {
+                    // TrackingToCharm
+                }
+                Sequence::SharmCollected => {
+                    // SharmCollected
+                }
+                _ => {
+                    // Default case
+                }
+            }
         }
     });
 
