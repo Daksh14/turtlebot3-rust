b6c0d6b908b406bd18dc1d82d65618f64ac55ec8
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-03-30 18:35:08 -0400

    Add lidar support

--- docker-compile.sh
+++ docker-compile.sh
@@ -2,7 +2,7 @@
 
 container_id=$(docker ps -q --filter "ancestor=my-system")
 
-rm -rf ./target
+sudo rm -rf ./target
 
 # Copy the project files to the container
 docker cp ./ $container_id:/rust-example/
