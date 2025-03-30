be3b4c42bae5ed9b9ea6913842d909a4c52635af
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-03-30 18:04:03 -0400

    Push binary

--- copy-result.sh
+++ copy-result.sh
@@ -2,8 +2,8 @@
 
 container_id=$(docker ps -q --filter "ancestor=my-system")
 
-rm -rf ./output/
-mkdir -p ./output
+rm -rf ./output-binary/
+mkdir -p ./output-binary
 
 # Copy the project files to the container
-docker cp $container_id:/rust-example/target/aarch64-unknown-linux-gnu/release/ros2_cmd_vel_publisher ./output/
+docker cp $container_id:/rust-example/target/aarch64-unknown-linux-gnu/release/ros2_cmd_vel_publisher ./output-binary/
