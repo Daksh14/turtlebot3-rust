14b3196ba0a3cfb33c16127ea76e3cb0dbe1fe50
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-03-30 19:57:42 -0400

    New bin

--- src/nav.rs
+++ src/nav.rs
@@ -47,31 +47,10 @@ pub async fn nav_move(publisher: &Publisher<Twist>, distance_x: f64, distance_y:
     // Sleep for time needed to reach distance
     sleep(Duration::from_secs(travel_time)).await;
 
-    // stop afterwards
-    let stop_twist = Twist {
-        linear: Vector3 {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-        }, // Move forward
-        angular: Vector3 {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-        }, // Rotate slightly
-    };
-
-    // Publish the stop message
-    match publisher.publish(&stop_twist) {
-        Ok(_) => println!("stopping"),
-        Err(e) => eprintln!(
-            "Failed to publish stop instructions, this is bad the bot will keep moving: {}",
-            e
-        ),
-    }
+    nav_stop(publisher);
 }
 
-pub fn rotate360(publisher: &Publisher<Twist>) {
+pub async fn rotate360(publisher: &Publisher<Twist>) {
     let twist = Twist {
         linear: Vector3 {
             x: 0.0,
@@ -90,4 +69,30 @@ pub fn rotate360(publisher: &Publisher<Twist>) {
         Ok(_) => println!("Rotating instruction sent"),
         Err(e) => eprintln!("Failed to publish 360 rotating instructions {}", e),
     }
+
+    // Sleep for time needed to reach distance
+    sleep(Duration::from_secs(5)).await;
+
+    nav_stop(publisher);
+}
+
+pub fn nav_stop(publisher: &Publisher<Twist>) {
+    let twist = Twist {
+        linear: Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        angular: Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        },
+    };
+
+    // Publish the rotation message
+    match publisher.publish(&twist) {
+        Ok(_) => println!("Stopping instruction sent"),
+        Err(e) => eprintln!("Failed to stop the bot, this is bad {}", e),
+    };
 }
