7da50914b8130c0f650899765d44bf3ce444aad4
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-03-30 21:02:03 -0400

    more Lidar stuff

--- src/lidar.rs
+++ src/lidar.rs
@@ -25,32 +25,58 @@ pub fn lidar_data(data: LaserScan) {
     let threshold = 0.5; // Distance threshold for detecting objects
     let len = lidar_data.len();
 
-    println!("{}", len);
+    let size_of_quater = 23;
 
-    // the bot is facing in a certain direction for us to determine the left, center, and right region
-    let bottom_left = &lidar_data[0..20];
-    let bottom_right = &lidar_data[20..40];
-    let right = &lidar_data[40..60];
-    let top_right = &lidar_data[60..80];
-    let top_left = &lidar_data[80..100];
-    let left = &lidar_data[100..120];
-    let bottom_left = &lidar_data[120..140];
-    let bottom_right = &lidar_data[140..160];
+    // there should be 8 quaters, the approx length of the lidar range is 203 elements
+    let quater_one = &lidar_data[0..size_of_quater];
+    let quater_two = &lidar_data[size_of_quater..size_of_quater * 2];
+    let quater_three = &lidar_data[size_of_quater * 2..size_of_quater * 3];
+    let quater_four = &lidar_data[size_of_quater * 3..size_of_quater * 4];
+    let quater_five = &lidar_data[size_of_quater * 4..size_of_quater * 5];
+    let quater_six = &lidar_data[size_of_quater * 5..size_of_quater * 6];
+    let quater_seven = &lidar_data[size_of_quater * 6..size_of_quater * 7];
+    let quater_eight = &lidar_data[size_of_quater * 7..size_of_quater * 8];
 
-    let min_bottom_left = find_n_min_values(bottom_left.to_vec());
-    let avg = find_average(min_bottom_left);
-    let mut close = false;
+    let min_and_avg_q_one = find_average(find_n_min_values(quater_one.to_owned()));
+    let min_and_avg_q_two = find_average(find_n_min_values(quater_two.to_owned()));
+    let min_and_avg_q_three = find_average(find_n_min_values(quater_three.to_owned()));
+    let min_and_avg_q_four = find_average(find_n_min_values(quater_four.to_owned()));
+    let min_and_avg_q_five = find_average(find_n_min_values(quater_five.to_owned()));
+    let min_and_avg_q_six = find_average(find_n_min_values(quater_six.to_owned()));
+    let min_and_avg_q_seven = find_average(find_n_min_values(quater_seven.to_owned()));
+    let min_and_avg_q_eight = find_average(find_n_min_values(quater_eight.to_owned()));
 
-    if avg < threshold {
-        close = true;
+    if min_and_avg_q_one < threshold {
+        println!("north");
     }
 
-    // Print results
-    if close {
-        println!("Object detected nearby on the bottom left side!");
-    } else {
-        println!("No close object detected.");
+    if min_and_avg_q_two < threshold {
+        println!("south");
     }
+
+    // if min_and_avg_q_three < threshold {
+    //     println!("east");
+    // }
+
+    // if min_and_avg_q_four < threshold {
+    //     println!("west");
+    // }
+
+    // if min_and_avg_q_five < threshold {
+    //     println!("north east");
+    // }
+
+    // if min_and_avg_q_six < threshold {
+    //     println!("north west");
+    // }
+
+    // if min_and_avg_q_seven < threshold {
+    //     println!("south east");
+    // }
+
+    // if min_and_avg_q_eight < threshold {
+    //     println!("south west");
+    // }
 }
 
 fn find_n_min_values(arr: Vec<f32>) -> Vec<f32> {
