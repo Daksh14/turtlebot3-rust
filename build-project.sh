01f89f0cd91678a5e0601468ae533076924067a4
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-04-01 01:45:55 -0400

    setup opencv and camera module

--- build-project.sh
+++ build-project.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-
+OPENCV_LINK_LIBS="opencv_imgproc,opencv_face,opencv_objdetect,opencv_dnn,opencv_dnn_objdetect,opencv_core,ippiw,ittnotify,ippicv,liblibprotobuf,z" \
+OPENCV_LINK_PATHS=/root/opencv/lib,/root/opencv/lib/opencv4/3rdparty,/usr/lib/aarch64-linux-gnu \
+OPENCV_INCLUDE_PATHS=/root/opencv/include/opencv4 \
 . /opt/ros/jazzy/setup.sh && cargo build --target=aarch64-unknown-linux-gnu --release
