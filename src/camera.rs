03e0a19eec7999cd3fb3c15f6b17ecd375eb60e8
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-04-02 10:55:54 -0400

    yolo and camera

--- src/camera.rs
+++ src/camera.rs
@@ -26,19 +26,19 @@ pub async fn cam_plus_yolo_detect() -> Result<()> {
     cam.set(videoio::CAP_PROP_FRAME_WIDTH, 640.0)?; // Set frame width
     cam.set(videoio::CAP_PROP_FRAME_HEIGHT, 480.0)?;
 
+    let fps_start = Instant::now();
+    let num_frames_to_measure = 60;
+    for _ in 0..num_frames_to_measure {
+        let mut temp_frame = Mat::default();
+        cam.read(&mut temp_frame)?;
+    }
+    let fps_duration = fps_start.elapsed();
+    let fps = num_frames_to_measure as f64 / fps_duration.as_secs_f64();
+    println!("Measured FPS: {:?}", fps);
+
     loop {
         let mut frame = Mat::default();
         cam.read(&mut frame)?;
-
-        frame_count += 1;
-
-        let elapsed = last_time.elapsed();
-        if elapsed.as_secs() >= 1 {
-            let fps = frame_count as f64 / elapsed.as_secs_f64();
-            println!("FPS: {:.2}", fps);
-            frame_count = 0;
-            last_time = Instant::now();
-        }
     }
 
     // tokio::spawn({
