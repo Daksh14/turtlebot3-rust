138153037678a5d137a40fde0a6d20488f503011
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-04-02 15:40:47 -0400

    yolo and camera

--- src/camera.rs
+++ src/camera.rs
@@ -12,6 +12,8 @@ pub async fn cam_plus_yolo_detect() -> Result<()> {
 
     let fourcc = VideoWriter::fourcc('M', 'J', 'P', 'G')?;
 
+    cam.set(videoio::CAP_PROP_FRAME_WIDTH, 640.0)?;
+    cam.set(videoio::CAP_PROP_FRAME_HEIGHT, 480.0)?;
     cam.set(videoio::CAP_PROP_FOURCC, fourcc as f64)?;
 
     let opened = VideoCapture::is_opened(&cam)?;
@@ -25,48 +27,24 @@ pub async fn cam_plus_yolo_detect() -> Result<()> {
     }
 
     tokio::spawn(async move {
-        let mut frame_count = 0;
-        let mut last_time = Instant::now();
-
         loop {
             let mut frame = Mat::default();
             cam.read(&mut frame).expect("should be able to read frame");
 
             tx.send(frame).await.expect("Should be able to send frame");
-
-            frame_count += 1;
-            let elapsed = last_time.elapsed();
-            if elapsed.as_secs() >= 1 {
-                let fps = frame_count as f64 / elapsed.as_secs_f64();
-                println!("true camera FPS: {:.2}", fps);
-                frame_count = 0;
-                last_time = Instant::now();
-            }
         }
     });
 
-    use std::time::Instant;
-
-    let mut frame_count = 0;
-    let mut last_time = Instant::now();
-
     loop {
         if let Some(x) = rx.recv().await {
-            frame_count += 1;
-            let elapsed = last_time.elapsed();
-            if elapsed.as_secs() >= 1 {
-                let fps = frame_count as f64 / elapsed.as_secs_f64();
-                println!("recving FPS: {:.2}", fps);
-                frame_count = 0;
-                last_time = Instant::now();
+            match yolo::detect(&mut model, &x, 0.5, 0.5) {
+                Ok(_) => {
+                    println!("Detected something");
+                }
+                Err(e) => {
+                    println!("error in deteching {:?}", e);
+                }
             }
-
-            // match yolo::detect(&mut model, &x, 0.5, 0.5) {
-            //     Ok(_) => {
-            //         println!("Detected something");
-            //     }
-            //     _ => (),
-            // }
         }
     }
 }
