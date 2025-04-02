0312dbf78dcd8bc1021602314017df8a7d3b6175
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-04-02 10:53:02 -0400

    yolo and camera

--- src/camera.rs
+++ src/camera.rs
@@ -6,19 +6,25 @@ use opencv::{
 
 use crate::yolo;
 
+use std::sync::Mutex;
 use std::time::Instant;
 
 pub async fn cam_plus_yolo_detect() -> Result<()> {
     let mut cam = VideoCapture::new(0, videoio::CAP_ANY)?;
+
     let opened = VideoCapture::is_opened(&cam)?;
     let mut frame_count = 0;
     let mut last_time = Instant::now();
     // load the yolo model
     let mut model = yolo::load_model().expect("The model should load");
+    let buffer: Mutex<Vec<Mat>> = Mutex::new(Vec::new());
 
     if !opened {
         panic!("Unable to open default camera!");
     }
+
+    cam.set(videoio::CAP_PROP_FRAME_WIDTH, 640.0)?; // Set frame width
+    cam.set(videoio::CAP_PROP_FRAME_HEIGHT, 480.0)?;
 
     loop {
         let mut frame = Mat::default();
@@ -34,4 +40,27 @@ pub async fn cam_plus_yolo_detect() -> Result<()> {
             last_time = Instant::now();
         }
     }
+
+    // tokio::spawn({
+    //     loop {
+    //         let mut frame = Mat::default();
+    //         cam.read(&mut frame)?;
+
+    //         if let Ok(mut lock) = buffer.lock() {
+    //             lock.push(frame);
+    //         }
+    //     }
+    // })
+
+    // loop {
+    //     frame_count += 1;
+
+    //     let elapsed = last_time.elapsed();
+    //     if elapsed.as_secs() >= 1 {
+    //         let fps = frame_count as f64 / elapsed.as_secs_f64();
+    //         println!("FPS: {:.2}", fps);
+    //         frame_count = 0;
+    //         last_time = Instant::now();
+    //     }
+    // }
 }
