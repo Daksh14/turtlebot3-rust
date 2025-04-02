d1d7bba79af8d70fe313ad12c545e0f886745b62
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-04-02 15:14:42 -0400

    yolo and camera

--- src/camera.rs
+++ src/camera.rs
@@ -3,60 +3,44 @@ use opencv::{
     Result,
     videoio::{self, VideoCapture, VideoWriter},
 };
+use tokio::sync::mpsc;
 
 use crate::yolo;
-
-use std::sync::Mutex;
-use std::time::Instant;
 
 pub async fn cam_plus_yolo_detect() -> Result<()> {
     let mut cam = VideoCapture::new(0, videoio::CAP_V4L)?;
 
     let fourcc = VideoWriter::fourcc('M', 'J', 'P', 'G')?;
 
-    // cam.set(videoio::CAP_PROP_FRAME_WIDTH, 640.0)?;
-    // cam.set(videoio::CAP_PROP_FRAME_HEIGHT, 480.0)?;
     cam.set(videoio::CAP_PROP_FOURCC, fourcc as f64)?;
 
     let opened = VideoCapture::is_opened(&cam)?;
-    let mut frame_count = 0;
-    let mut last_time = Instant::now();
+
     // load the yolo model
     let mut model = yolo::load_model().expect("The model should load");
-    let buffer: Mutex<Vec<Mat>> = Mutex::new(Vec::new());
+    let (tx, mut rx) = mpsc::channel::<Mat>(100);
 
     if !opened {
         panic!("Unable to open default camera!");
     }
 
-    // loop {
-    //     let mut frame = Mat::default();
-    //     cam.read(&mut frame)?;
-    // }
+    tokio::spawn(async move {
+        loop {
+            let mut frame = Mat::default();
+            cam.read(&mut frame).expect("should be able to read frame");
 
-    // tokio::spawn({
-    //     loop {
-    //         let mut frame = Mat::default();
-    //         cam.read(&mut frame)?;
-
-    //         if let Ok(mut lock) = buffer.lock() {
-    //             lock.push(frame);
-    //         }
-    //     }
-    // })
+            tx.send(frame).await.expect("Should be able to send frame");
+        }
+    });
 
     loop {
-        let mut frame = Mat::default();
-        cam.read(&mut frame)?;
-
-        frame_count += 1;
-
-        let elapsed = last_time.elapsed();
-        if elapsed.as_secs() >= 1 {
-            let fps = frame_count as f64 / elapsed.as_secs_f64();
-            println!("FPS: {:.2}", fps);
-            frame_count = 0;
-            last_time = Instant::now();
+        if let Some(x) = rx.recv().await {
+            match yolo::detect(&mut model, &x, 0.5, 0.5) {
+                Ok(_) => {
+                    println!("Detected something");
+                }
+                _ => (),
+            }
         }
     }
 }
