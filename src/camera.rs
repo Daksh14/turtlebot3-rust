876aabcf77dc8ccf305ddf76326ace871b8d7fa6
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-04-02 15:51:37 -0400

    yolo and camera

--- src/camera.rs
+++ src/camera.rs
@@ -5,6 +5,8 @@ use opencv::{
 };
 use tokio::sync::mpsc;
 
+use std::time::Instant;
+
 use crate::yolo;
 
 pub async fn cam_plus_yolo_detect() -> Result<()> {
@@ -36,8 +38,24 @@ pub async fn cam_plus_yolo_detect() -> Result<()> {
         }
     });
 
+    let mut frame_count = 0;
+    let mut last_time = Instant::now();
     loop {
         if let Some(x) = rx.recv().await {
+            frame_count += 1;
+
+            let elapsed = last_time.elapsed();
+
+            if elapsed.as_secs() >= 1 {
+                let fps = frame_count as f64 / elapsed.as_secs_f64();
+
+                println!("FPS: {:.2}", fps);
+
+                frame_count = 0;
+
+                last_time = Instant::now();
+            }
+
             match yolo::detect(&mut model, &x, 0.5, 0.5) {
                 Ok(_) => {
                     println!("Detected something");
