8532802586e0dc25e4ae132e9f163ef13ce142b1
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-04-05 14:32:33 -0400

    yolo and camera

--- build-project.sh
+++ build-project.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ORT_LIB_LOCATION=/onnxruntime/build/Linux/Release \
-. /opt/ros/jazzy/setup.sh && cargo run --target=aarch64-unknown-linux-gnu --release
+. /opt/ros/jazzy/setup.sh && cargo build --target=aarch64-unknown-linux-gnu --release
