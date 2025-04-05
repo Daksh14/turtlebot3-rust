8936205a781c0d3de12b810a6a0c6cd74117380b
Daksh <41485688+Daksh14@users.noreply.github.com>
2025-04-05 14:15:17 -0400

    yolo and camera

--- src/camera.rs
+++ src/camera.rs
@@ -5,50 +5,10 @@ use nokhwa::{
         ApiBackend, CameraIndex, FrameFormat, RequestedFormat, RequestedFormatType, Resolution,
     },
 };
-use ort::value::Tensor;
-use rgb::FromSlice;
-use std::time::Instant;
 use tokio::sync::mpsc;
 
 use crate::yolo::{self};
 
-use resize::px::RGB;
-
-struct U8ToF32;
-
-impl resize::PixelFormat for U8ToF32 {
-    type InputPixel = RGB<u8>;
-    type OutputPixel = RGB<f32>;
-    type Accumulator = RGB<f32>;
-
-    #[inline(always)]
-    fn new() -> Self::Accumulator {
-        RGB::new(0., 0., 0.)
-    }
-
-    #[inline(always)]
-    fn add(&self, acc: &mut Self::Accumulator, inp: RGB<u8>, coeff: f32) {
-        acc.r += inp.r as f32 * coeff;
-        acc.g += inp.g as f32 * coeff;
-        acc.b += inp.b as f32 * coeff;
-    }
-
-    #[inline(always)]
-    fn add_acc(acc: &mut Self::Accumulator, inp: Self::Accumulator, coeff: f32) {
-        acc.r += inp.r * coeff;
-        acc.g += inp.g * coeff;
-        acc.b += inp.b * coeff;
-    }
-
-    #[inline(always)]
-    fn into_pixel(&self, acc: Self::Accumulator) -> RGB<f32> {
-        RGB {
-            r: acc.r / 255.0,
-            g: acc.g / 255.0,
-            b: acc.b / 255.0,
-        }
-    }
-}
 
 pub async fn cam_plus_yolo_detect() -> Result<(), ()> {
     let mut model = yolo::load_model().expect("The model should load");
@@ -69,46 +29,12 @@ pub async fn cam_plus_yolo_detect() -> Result<(), ()> {
         })
         .expect("setting res should work");
 
-    let res = camera.resolution();
-    let width = res.width() as usize;
-    let height = res.height() as usize;
-
-    let mut input_img_buffer = vec![0u8; width * height * 3];
-    let mut resized_input = Tensor::from_array(([1i64, 3, 640, 640], vec![0_f32; 3 * 640 * 640]))
-        .expect("Should construct tensor");
-
-    let mut resizer = resize::new(width, height, 640, 640, U8ToF32, resize::Type::Triangle)
-        .expect("resizer should init");
 
     camera.open_stream().expect("Stream should start");
-
-    // load the yolo model
-    // let img_path = "./data/test.jpg"; // change the path if needed
-    // let img = ImageReader::open(img_path).unwrap().decode().unwrap();
-
-    // let mut xy = Tensor::from_array(([1i64, 3, 640, 640], vec![0f32; 3 * 640 * 640]))
-    //     .expect("Should construct tensor");
-
-    // let img = img.to_rgb32f();
-
-    // let raw = img.clone().into_vec();
-
-    // {
-    //     let (_, resized_input_buffer) = xy.extract_raw_tensor_mut();
-
-    //     resizer
-    //         .resize(raw.as_rgb(), resized_input_buffer.as_rgb_mut())
-    //         .expect("resize should work");
-    // }
-
-    // println!("yolo detect test {:?}", yolo::detect(&mut model, xy));
 
     let (tx, mut rx) = mpsc::channel::<Buffer>(100);
 
     tokio::spawn(async move {
-        let mut frame_count = 0;
-        let mut last_time = Instant::now();
-
         loop {
             let buffer = camera.frame().expect("frame should be retrievable");
 
@@ -119,22 +45,24 @@ pub async fn cam_plus_yolo_detect() -> Result<(), ()> {
 
     loop {
         if let Some(buffer) = rx.recv().await {
-            buffer
-                .decode_image_to_buffer::<RgbFormat>(&mut input_img_buffer)
+            let img = buffer
+                .decode_image::<RgbFormat>()
                 .expect("decoding imgae to buffer should work");
 
-            {
-                let (_, resized_input_buffer) = resized_input.extract_raw_tensor_mut();
-
-                resizer
-                    .resize(input_img_buffer.as_rgb(), resized_input_buffer.as_rgb_mut())
-                    .expect("resize should work");
-            }
-
-            match yolo::detect(&mut model, resized_input.view()) {
+            match yolo::detect(&mut model, img) {
                 Ok(_) => (),
                 Err(e) => println!("err: {:?}", e),
             }
         }
     }
+}
+
+pub async fn yolo_detect_test() {
+    let mut model = yolo::load_model().expect("The model should load");
+
+    // load the yolo model
+    let img_path = "./data/test.jpg"; // change the path if needed
+    let img = image::ImageReader::open(img_path).unwrap().decode().unwrap();
+
+    println!("yolo detect test {:?}", yolo::detect(&mut model, img.into()));
 }
--- src/yolo.rs
+++ src/yolo.rs
@@ -1,14 +1,11 @@
 // yolo.rs
-use ndarray::{s, Axis};
-use ort::{
-    session::{builder::SessionBuilder, Session},
-    value::{TensorValueType, ValueRef},
-};
 use serde::{Deserialize, Serialize};
+use usls::{models::YOLO, Device, Options, Vision, YOLOTask, YOLOVersion};
 
+use image::{DynamicImage, RgbImage};
 use std::{error::Error, fs::File, io::BufReader};
 
-pub type Frame<'a> = ValueRef<'a, TensorValueType<f32>>;
+pub type Frame = RgbImage;
 
 const YOLOV8_CLASS_LABELS: [&str; 10] = [
     "blue cone",
@@ -48,19 +45,33 @@ pub struct ModelConfig {
 }
 
 pub struct Model {
-    pub model: Session,
+    pub model: YOLO,
 }
 
 pub fn load_model() -> Result<Model, Box<dyn Error>> {
     let model_config = load_model_from_config().unwrap();
-    let model = SessionBuilder::new()?.commit_from_file(&model_config.model_path)?;
+
+    let options = Options::new()
+        .with_model(&model_config.model_path)
+        .expect("model should load")
+        .with_yolo_version(YOLOVersion::V8)
+        .with_yolo_task(YOLOTask::Detect)
+        .with_device(Device::Cpu(0))
+        .with_ixx(0, 0, (1, 1, 4).into())
+        .with_ixx(0, 2, (0, 640, 640).into())
+        .with_ixx(0, 3, (0, 640, 640).into())
+        .with_confs(&[0.2, 0.15])
+        .with_names(&YOLOV8_CLASS_LABELS);
+
+    let model = YOLO::new(options).expect("yolo model to load");
+
     println!("Yolo ONNX model loaded.");
 
     Ok(Model { model })
 }
 
 fn load_model_from_config() -> Result<ModelConfig, Box<dyn Error>> {
-    let file = File::open("../data/config.json"); // change the path if needed
+    let file = File::open("./data/config.json"); // change the path if needed
     let file = match file {
         Ok(file) => file,
         Err(e) => {
@@ -94,83 +105,10 @@ fn load_model_from_config() -> Result<ModelConfig, Box<dyn Error>> {
 // yolo.rs
 pub fn detect(model_data: &mut Model, img: Frame) -> Result<(), Box<dyn std::error::Error>> {
     let model = &mut model_data.model;
-    let model_inputs = [ort::session::SessionInputValue::from(img)];
-    let outputs = model.run(model_inputs).expect("inference should work");
 
-    let output = outputs["output0"]
-        .try_extract_tensor::<f32>()?
-        .t()
-        .into_owned();
-
-    let output = output.slice(s![.., .., 0]);
-    let img_width = 640;
-    let img_height = 640;
-    let mut boxes = Vec::new();
-
-    for row in output.axis_iter(Axis(0)) {
-        let row: Vec<_> = row.iter().copied().collect();
-        let (class_id, prob) = row
-            .iter()
-            // skip bounding box coordinates
-            .skip(4)
-            .enumerate()
-            .map(|(index, value)| (index, *value))
-            .reduce(|accum, row| if row.1 > accum.1 { row } else { accum })
-            .unwrap();
-
-        if prob < 0.5 {
-            continue;
-        }
-
-        let label = YOLOV8_CLASS_LABELS[class_id];
-
-        let xc = row[0] / 640. * (img_width as f32);
-        let yc = row[1] / 640. * (img_height as f32);
-        let w = row[2] / 640. * (img_width as f32);
-        let h = row[3] / 640. * (img_height as f32);
-
-        boxes.push((
-            BoundingBox {
-                x1: xc - w / 2.,
-                y1: yc - h / 2.,
-                x2: xc + w / 2.,
-                y2: yc + h / 2.,
-            },
-            label,
-            prob,
-        ));
-    }
-
-    boxes.sort_by(|box1, box2| box2.2.total_cmp(&box1.2));
-    let mut result = Vec::new();
-
-    while !boxes.is_empty() {
-        result.push(boxes[0]);
-        boxes = boxes
-            .iter()
-            .filter(|box1| intersection(&boxes[0].0, &box1.0) / union(&boxes[0].0, &box1.0) < 0.7)
-            .copied()
-            .collect();
-    }
+    let result = model.run(&[DynamicImage::ImageRgb8(img)]);
 
     println!("result: {:?}", result);
 
     Ok(())
-}
-
-#[derive(Debug, Clone, Copy)]
-struct BoundingBox {
-    x1: f32,
-    y1: f32,
-    x2: f32,
-    y2: f32,
-}
-
-fn intersection(box1: &BoundingBox, box2: &BoundingBox) -> f32 {
-    (box1.x2.min(box2.x2) - box1.x1.max(box2.x1)) * (box1.y2.min(box2.y2) - box1.y1.max(box2.y1))
-}
-
-fn union(box1: &BoundingBox, box2: &BoundingBox) -> f32 {
-    ((box1.x2 - box1.x1) * (box1.y2 - box1.y1)) + ((box2.x2 - box2.x1) * (box2.y2 - box2.y1))
-        - intersection(box1, box2)
 }
